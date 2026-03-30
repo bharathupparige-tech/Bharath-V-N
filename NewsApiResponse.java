1100dd549ee37301c402489867b14ebc7e16558f
bharathupparige-tech <bharathupparige@gmail.com>
2026-03-30 12:14:54 +0530

    Update NewsApiResponse.java

---
+++ NewsApiResponse.java
@@ -0,0 +1,47 @@
+package com.crio.xnews;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewsApiResponse {
+    private String status;
+    private int totalResults;
+    private List<NewsArticle> articles = new ArrayList<>();
+
+// TODO: CRIO_TASK_MODULE_PROJECT
+// Use your IDE to generate getters and setters for the variables declared above.
+// Use your IDE to generate the toString method to provide a string representation of the variables.
+public NewsApiResponse() {}
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public List<NewsArticle> getArticles() {
+        return articles;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setTotalResults(int totalResults) {
+        this.totalResults = totalResults;
+    }
+
+    public void setArticles(List<NewsArticle> articles) {
+        this.articles = articles;
+    }
+
+    @Override
+    public String toString() {
+        return "NewsApiResponse [status=" + status +
+                ", totalResults=" + totalResults +
+                ", articles=" + articles + "]";
+    }
+
+}
