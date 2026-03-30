1103282cc32dfa2a643ef363ccfca134f804cf3c
bharathupparige-tech <bharathupparige@gmail.com>
2026-03-30 12:06:33 +0530

    Update App.java

--- App.java
+++ App.java
@@ -1,1 +1,27 @@
+package com.crio.xnews;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class App {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static List<UserPreference> readUserPreferences(String filePath)
+            throws IOException {
+
+        return objectMapper.readValue(
+                new File(filePath),
+                new TypeReference<List<UserPreference>>() {}
+        );
+    }
+
+    // REQUIRED for Gradle run task
+    public static void main(String[] args) {
+        System.out.println("Hello World from XNews!");
+    }
+}
